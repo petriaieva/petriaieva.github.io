63ddb4bbe3949f5dab3273602ad2679b55abf987
Ievgeniia Petriaieva <pevgeniya20@gmail.com>
2017-05-12 11:30:01 +0200

    add new site MoGo for portfolio

---
+++ Mogo/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+
+
+}
+$(document).ready(function(){
+    $('#myModal').on('shown.bs.modal', function () {
+        initMap();
+  
+})
+});
